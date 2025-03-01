9fe52b9e2c0fae29f52c2a11bf029c616807cc8b
Aloxaf Yin <Aloxafx@gmail.com>
2025-03-01 13:23:13 +0800

    fix: simplify

--- functions/api/[[route]].ts
+++ functions/api/[[route]].ts
@@ -10,34 +10,25 @@ import { handle } from 'hono/cloudflare-pages';
 const app = new Hono();
 
 app.post('/api/v1', async (c) => {
-  const headers: HeadersInit = {}
-
   const settings = getPluginSettingsFromRequest(c.req.raw)
-  const apiKey = settings?.JINA_API_KEY;
-  if (apiKey) {
-    headers['Authorization'] = `Bearer ${apiKey}`
+  if (!settings) {
+    return createErrorResponse(PluginErrorType.PluginSettingsInvalid, {
+      message: 'Invalid plugin settings'
+    });
   }
+
+  const apiKey = settings?.JINA_API_KEY;
 
   const { url } = await c.req.json();
   const response = await fetch(`https://r.jina.ai/${url}`, {
     method: 'GET',
-    headers
-  })
-
-  if (!response.ok) {
-    if (response.status === 429 && !apiKey) {
-      return createErrorResponse(PluginErrorType.PluginSettingsInvalid, {
-        message: 'Rate limit exceeded. Please provide your API key.'
-      });
-    } else {
-      return createErrorResponse(PluginErrorType.PluginServerError, {
-        message: `Failed to fetch data from Jina AI: ${response.statusText}`
-      });
+    headers: {
+      'Authorization': `Bearer ${apiKey}`
     }
-  }
+  });
 
   const text = await response.text();
-  return c.text(text)
+  return c.text(text);
 });
 
 export const onRequest = handle(app);
