27b5963405035f20246a49ee8fe6aec1e1a966a8
Aloxaf Yin <Aloxafx@gmail.com>
2025-03-03 14:11:43 +0800

    feat: 搜索多个链接

--- functions/api/[[route]].ts
+++ functions/api/[[route]].ts
@@ -18,17 +18,45 @@ app.post('/api/v1', async (c) => {
   }
 
   const apiKey = settings?.JINA_API_KEY;
-
   const { url } = await c.req.json();
+
   const response = await fetch(`https://r.jina.ai/${url}`, {
     method: 'GET',
     headers: {
-      'Authorization': `Bearer ${apiKey}`
+      'Authorization': `Bearer ${apiKey}`,
+      'Accept': 'application/json',
     }
   });
 
-  const text = await response.text();
-  return c.text(text);
+  const json: any = await response.json();
+  return c.json(json);
+});
+
+app.post('/api/v2/multiWebReader', async (c) => {
+  const settings = getPluginSettingsFromRequest(c.req.raw)
+  if (!settings) {
+    return createErrorResponse(PluginErrorType.PluginSettingsInvalid, {
+      message: 'Invalid plugin settings'
+    });
+  }
+
+  const apiKey = settings?.JINA_API_KEY;
+  const { urls } = await c.req.json();
+
+  const result = await Promise.all(urls.map(async (url: string) => {
+    const response = await fetch(`https://r.jina.ai/${url}`, {
+      method: 'GET',
+      headers: {
+        'Authorization': `Bearer ${apiKey}`,
+        'Accept': 'application/json',
+      }
+    });
+
+    const json: any = await response.json();
+    return json;
+  }));
+
+  return c.json(result);
 });
 
 export const onRequest = handle(app);
